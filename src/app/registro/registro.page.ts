841946e8218c2cafac3ab317a4b37288f126d0cf
aryflavio <flavioarianny@gmail.com>
2022-11-24 10:51:27 -0300

    colocando requerimento

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -10,6 +10,30 @@ export class RegistroPage implements OnInit {
 
   formCadastro: FormGroup;
 
+  mensgaens = {
+    nome: [
+      {tipo:'required', mensagem:'O Campo Nome é Obrigatório'},
+      {tipo:'minlength', mensagem: 'O campo Nome precisa ter pelo menos 3 caracteres!'}
+    ],
+    cpf:[
+      {tipo:'required', mensagem: 'O campo CPF é obrigatório"'}
+    ],
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+      { tipo: 'email', mensagem: 'E-mail Inválido.' },
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+    ],
+    confirmaSenha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+    ],
+  };
+
   constructor(private formBuilder: FormBuilder) { this.formCadastro = this.formBuilder.group({
       nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
       cpf: ['', Validators.compose([Validators.required])],
