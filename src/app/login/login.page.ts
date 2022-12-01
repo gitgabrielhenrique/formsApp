cb50341f22252c902375706572bda2f3563350a7
Gabriel Henrique dos Santos <gabrielHsantos2018@gmail.com>
2022-12-01 08:59:39 -0300

    LoginPage

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,9 @@
+import { Storage } from '@ionic/storage-angular';
+import { StorageService } from './../services/storage.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from '../models/Usuario';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  formLogin: FormGroup;
+  mensagens = {
+    email: [{ tipo: 'required', mensagem: 'O Campo email é Obrigatório' }],
+    senha: [{ tipo: 'required', mensagem: 'O Campo senha é Obrigatório' }]
+  };
+  constructor(
+    private formBuilder: FormBuilder,
+    private storageService: StorageService,
+    private route: Router
+  ) {
+    this.formLogin = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(8),
+        ]),
+      ],
+    });
   }
 
+  ngOnInit() {}
 }
